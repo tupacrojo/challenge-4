a59319475c4aaa2cb2ee3ae9c08b0c4119d0cf30
Tupac Rodriguez <tupacrojo@live.com>
2022-05-05 12:11:58 -0300

    Agregar archivos de proyecto.

---
+++ challenge 4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace challenge_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 3;
+            Persona[] personas = new Persona[total];
+
+            for (int i = 0; i < total; i++)
+            {
+                Console.WriteLine("Ingrese el nombre N°" + (i+1) + ":");
+                personas[i] = new Persona()
+                {
+                    Nombre = Console.ReadLine()
+                };
+            }
+
+            for (int i = 0; i < total; i++)
+            {
+                Console.WriteLine(personas[i].ToString());
+            }
+        }
+
+        public class Persona
+        {
+            public string Nombre { get; set; }
+
+            public override string ToString()
+            {
+                return "¡Hola! Mi nombres es " + Nombre;
+            }
+        }
+    }
+}
